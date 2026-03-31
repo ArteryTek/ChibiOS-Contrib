bc6b4b1977ff1615ebb530735514c46adab46914
jun tan <juntan@arterytek.com>
2026-03-31 11:34:37 +0800

    AT32F402/405 Add delay to write CRM_BPDC

--- os/hal/ports/AT32/AT32F402xx/hal_lld.c
+++ os/hal/ports/AT32/AT32F402xx/hal_lld.c
@@ -61,7 +61,9 @@ static void hal_lld_backup_domain_init(void) {
   if ((CRM->BPDC & AT32_ERTCSEL_MASK) != AT32_ERTCSEL) {
     /* Backup domain reset.*/
     CRM->BPDC = CRM_BPDC_BPDRST;
+    __NOP();__NOP();__NOP();__NOP();__NOP();__NOP();
     CRM->BPDC = 0;
+    __NOP();__NOP();__NOP();__NOP();__NOP();__NOP();
   }
 
   /* If enabled then the LEXT is started.*/
@@ -81,9 +83,11 @@ static void hal_lld_backup_domain_init(void) {
      initialization.*/
   if ((CRM->BPDC & CRM_BPDC_ERTCEN) == 0) {
     /* Selects clock source.*/
+    __NOP();__NOP();__NOP();__NOP();__NOP();__NOP();
     CRM->BPDC |= AT32_ERTCSEL;
 
     /* ERTC clock enabled.*/
+    __NOP();__NOP();__NOP();__NOP();__NOP();__NOP();
     CRM->BPDC |= CRM_BPDC_ERTCEN;
   }
 #endif /* AT32_ERTCSEL != AT32_ERTCSEL_NOCLOCK */
